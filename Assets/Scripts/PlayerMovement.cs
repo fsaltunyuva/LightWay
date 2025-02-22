fe4db3c0bc7c9533f37110915fd4a839e15ae1e7
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2025-02-22 20:28:35 +0300

    Basic Jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 1000f; // Speed variable
+    public float jumpSpeed = 100f; // Jump speed variable
     public Rigidbody2D rb; // Set the variable 'rb' as Rigibody
     public Vector2 movement;
     [SerializeField] private GameObject yamuk;
     public bool amIFacingLeft = false;
     private Animator _animator;
+    private bool grounded = true;
     
     // Start is called before the first frame update
     void Start()
@@ -24,12 +26,22 @@ public class PlayerMovement : MonoBehaviour
         Run();
         FlipSprite();
         _animator.SetBool("walk", movement != Vector2.zero);
+        Jump();
+        
     }
 
     private void Run(){
         Vector2 playerVelocity = new Vector2(movement.x * speed * Time.deltaTime, rb.velocity.y);
         rb.velocity = playerVelocity;
-        
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(grounded)
+                rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        }
     }
 
     private void FlipSprite()
@@ -47,5 +59,23 @@ public class PlayerMovement : MonoBehaviour
             amIFacingLeft = true;
         }
             
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("Grounded");
+            grounded = true;
+        }
+    }
+    
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("Not Grounded");
+            grounded = false;
+        }
     }
 }
