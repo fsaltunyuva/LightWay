533f6c76190cf97d054e24e7eeffacabea24cb37
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2025-02-22 12:52:50 +0300

    Flashlight Rotate

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 1000f; // Speed variable
     public Rigidbody2D rb; // Set the variable 'rb' as Rigibody
     public Vector2 movement;
+
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +22,21 @@ public class PlayerMovement : MonoBehaviour
     {
         movement = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
         Run();
+        FlipSprite();
     }
 
     private void Run(){
         Vector2 playerVelocity = new Vector2(movement.x * speed * Time.deltaTime, rb.velocity.y);
         rb.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        if(movement == Vector2.zero) return;
+        
+        if (movement.x > 0)
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        else if (movement.x < 0)
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
     }
 }
