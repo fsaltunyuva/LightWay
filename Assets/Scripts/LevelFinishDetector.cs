115f466156bed407e442efcb4daede955d2930cd
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2025-02-23 14:46:18 +0300

    Door Collision Fix

---
+++ Assets/Scripts/LevelFinishDetector.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelFinishDetector : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Door"))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 4)
+            {
+                SceneManager.LoadScene(0);
+                return;
+            }
+            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            
+        }
+    }
+}
